dd7073cb8414b38e7fc0fd20a24370be3c8d70d9
jxwleong <jason.xie.wei.leong@gmail.com>
2021-02-26 19:49:19 +0800

    Added procs in text_editor.sh to remove the text file variable

--- text_editor.sh
+++ text_editor.sh
@@ -8,3 +8,26 @@ proc read_file {filepath} {
 }
 
 read_file "local.txt"
+
+proc remove_list_element {list element} {
+	set idx [lsearch $list $element]
+	set list [lreplace $list $idx $idx]
+	return $list
+}
+
+proc process_file_data {filepath} {
+    set fp [open $filepath r]
+    set file_data [read $fp]
+    close $fp
+
+	set data [split $file_data "\n"]
+	set data [remove_list_element $data "NewCpuSpeed=*"]
+	puts $data
+	#	foreach line $data {
+#		puts "1$data"
+#	}
+
+}
+
+process_file_data "local.txt"
+
